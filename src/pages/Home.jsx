32856f0081efe4a5fb9cca41906cd96fc26367fc
dhruvg24 <dhruvgarg24112002@gmail.com>
2025-07-05 15:48:52 +0530

    skills section completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Background from '../components/Background';
 import Navbar from '../components/Navbar';
 import HeroSection from '../components/HeroSection';
 import AboutSection from '../components/AboutSection';
+import SkillsSection from '../components/SkillsSection';
 
 const Home = () => {
   return (
@@ -25,6 +26,7 @@ const Home = () => {
         <main>
           <HeroSection />
           <AboutSection />
+          <SkillsSection />
         </main>
 
 
